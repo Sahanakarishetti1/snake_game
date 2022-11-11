b2af990cae2f1f2cde9dd1ae56d93585d2e3b2a4
Sahana Karishetti <sahanakarishetti@gmail.com>
2022-11-11 11:28:03 +0530

    compplete

--- main.py
+++ main.py
@@ -36,18 +36,25 @@ screen.onkey(snake.right, "Right")
 game_is_on=True
 while(game_is_on):
     screen.update()
-    time.sleep(0.5)
+    time.sleep(1)
     Snake.move(snake)
 #detect collision of food
     if snake.head.distance(food) <15:
         food.refresh()
+        snake.extend()
         scorecard.scoreincrease()
     #detect collision of wall
     if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
         game_is_on=False
         scorecard.game_over()
 
-
+    #detect snake collision with its own head
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
 
